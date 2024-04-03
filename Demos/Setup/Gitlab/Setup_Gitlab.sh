1cc99775d7a8c02aca7f8ebed23c878067df1fd6
Travis Michette <tmichett@redhat.com>
2024-04-03 17:11:25 -0400

    Updated files based on Jenkins build in test.txt

--- Demos/Setup/Gitlab/Setup_Gitlab.sh
+++ Demos/Setup/Gitlab/Setup_Gitlab.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 echo "**********************************************"
+echo "***** Copying Collections ********************"
+cp collections/*.tar.gz /tmp
+
+echo "**********************************************"
+echo "***** Installing Collections ********************"
+ansible-galaxy collection install -r collections/requirements.yml -p collections/
+
+
+echo "**********************************************"
 echo "***** Clone Repo from Github *****************"
 
 git config --global credential.helper store
 mkdir /tmp/Github
 cd /tmp/Github
-git clone https://github.com/tmichett/AAP2_Demos.git
-cd /tmp/Github/AAP2_Demos
+git clone https://github.com/tmichett/do274_eda_rulebooks.git
+cd /tmp/Github/do274_eda_rulebooks
 
 
 echo "**********************************************"
@@ -18,44 +27,26 @@ echo ""
 
 echo "Creating Gitlab Project"
 
-cd /home/student/Github/DO467_Demo/Demos/Demo_Setup/Gitlab
+cd /home/student/Github/DO274_Demo/Demos/Setup/Gitlab
 
 #Install Pre-Req Python Modules for Gitlab Ansible Modules
-sudo python3.6 -m pip install python-gitlab
-sudo python3.6 -m pip install requests
+sudo python -m pip install python-gitlab
+sudo python -m pip install requests
 
 #Install Community General Colelction for Ansible to provide Gitlab Ansible Module
 ansible-galaxy collection install collections/community-general-5.5.0.tar.gz -p ./collections/
 
 #Run Ansible Playbook with Correct Python Interpreter
-ansible-playbook Create_Gitlab_Project.yml  -e 'ansible_python_interpreter=/usr/bin/python3.6'
+ansible-playbook Create_Gitlab_Project.yml  -e 'ansible_python_interpreter=/usr/bin/python'
 
-
-#echo "Please Setup the Gitlab Repository and Create in Gitlab WebUI"
-#echo "URL: https://git.lab.example.com"
-#echo "UN/PW: student/Student@123"
-#echo "Select *Create Project => Create Blank Project*"
-#echo "Project Name: AAP2_Demos"
-#echo "Visibility: Public"
-#echo "Click *Create Project*"
-#echo "Press Enter key to continue (After Repository Setup)"
-#while [ true ] ; do
-#read -t 3 -n 1
-#if [ $? = 0 ] ; then
-#break ;
-#else
-#echo "waiting for the Gitlab Setup"
-#fi
-#done
-
-echo "Pushing AAP2 Demo to Gitlab Now"
+echo "Pushing DO274 Demo to Gitlab Now"
 echo "UN/PW Combination"
 echo "UN: student"
-echo "PW: Student@123"
+echo "PW: Stud3nt123"
 
-cd /tmp/Github/AAP2_Demos
-git remote add gitlab https://git.lab.example.com/student/aap2_demos
-git remote set-url origin https://"student:Student@123"@git.lab.example.com/student/aap2_demos
+cd /tmp/Github/do274_eda_rulebooks
+git remote add gitlab https://git.lab.example.com/student/DO274_Demo
+git remote set-url origin https://"student:Stud3nt123"@git.lab.example.com/student/DO274_Demo
 git push -u gitlab main
 
 
@@ -66,19 +57,12 @@ echo "**********************************************"
 
 mkdir /home/student/Gitlab
 cd /home/student/Gitlab
-git clone https://git.lab.example.com/student/aap2_demos.git
+git clone https://git.lab.example.com/student/DO274_Demo.git
 
 
 echo "**********************************************"
-echo "******* Inventory Demo Setup Gitlab *   ******"
+echo "***** Setting Up Ansible Controller  *********"
 echo "**********************************************"
 
-mkdir /tmp/CH7
-cp -avR /home/student/Github/DO467_Demo/Demos/CH7/Inventory_Project/* /tmp/CH7/
-cd ~/Github
-git clone https://git.lab.example.com/student/Inv_Proj
-cd Inv_Proj
-cp -avR /tmp/CH7/* .
-git add .
-git commit -m "Initial Inventory Add"
-git push
+ansible-playbook Create_Project.yml
+ansible-playbook Create_Jobs.yml
