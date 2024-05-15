bdac0a5755d6802691007f8bfbd9789f78fb9983
Travis Michette <tmichett@redhat.com>
2024-05-15 10:35:38 -0400

    Updated files based on Jenkins build in test.txt

---
+++ Demos/CH3/URL_Check_MM_Demo/Setup_Demo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "**********************************************"
+echo "***** Copying Collections ********************"
+cp collections/*.tar.gz /tmp
+
+echo "**********************************************"
+echo "***** Installing Collections ********************"
+ansible-galaxy collection install -r collections/requirements.yml -p collections/
+
+
+echo "**********************************************"
+echo "***** Configuring ServerA ********************"
+ansible-playbook Setup_Webserver.yml
+lab start example-chat
+
